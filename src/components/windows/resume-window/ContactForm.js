a93939c39169bfc8d3c538cbfe3b903818b0d659
Tymotex <timzhang3@gmail.com>
2020-12-12 14:09:05 +1100

    Change input type from hidden to text

--- src/components/windows/resume-window/ContactForm.js
+++ src/components/windows/resume-window/ContactForm.js
@@ -7,11 +7,11 @@ const ContactForm = () => {
             <input type="hidden" name="form-name" value="Contact Form" />
             <div className="field half first">
                 <label htmlFor="name">Your Name</label>
-                <input type="hidden" name="name" id="name" />
+                <input type="text" name="name" id="name" />
             </div>
             <div className="field half">
                 <label htmlFor="email">Your Email</label>
-                <input type="hidden" name="email" id="email" />
+                <input type="text" name="email" id="email" />
             </div>
             <div className="field">
                 <label htmlFor="message">Message</label>
