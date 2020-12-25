40d9856c3a5598676e02df14ed7c8fdb5019b6b3
Tymotex <timzhang3@gmail.com>
2020-12-25 11:54:39 +1100

    Added transparency to hover overlay, added tactile ds gif

--- src/components/windows/projects-window/project-card/HoverOverlay.js
+++ src/components/windows/projects-window/project-card/HoverOverlay.js
@@ -6,12 +6,12 @@ import windowStyles from '../../Window.module.scss';
 const HoverOverlay = ({ icon, label }) => {
     return (
         <span className={windowStyles.overlay}>
-            <img src={icon} alt="linked site" style={{"pointer-events": "none"}}/>
+            <img className={overlayStyles.image} src={icon} alt="linked site"/>
             <Typography className={overlayStyles.label}  style={{"pointer-events": "none"}}  variant="h6" component="h2">
                 {label}
             </Typography>
         </span>
     );
-}
+};
 
 export default HoverOverlay;
