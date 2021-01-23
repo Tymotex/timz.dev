47abd6185495d991b869d91cbfa2cce061818428
Tymotex <timzhang3@gmail.com>
2021-01-23 15:30:01 +1100

    Temporarily cleared  graphql variable (causing build failure)

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,38 +2,41 @@ import { graphql } from "gatsby";
 import React from "react";
 import HtmlToReact from "html-to-react"
 
-export const query = graphql`
-    query ($slug: String!) {
-        markdownRemark (
-            fields: {
-                slug: {
-                    eq: $slug
-                }
-            }
-        ) {
-            html,
-            timeToRead,
-            wordCount {
-                words
-            }
-        }
-    }
-`;
+// export const query = graphql`
+//     query ($slug: String!) {
+//         markdownRemark (
+//             fields: {
+//                 slug: {
+//                     eq: $slug
+//                 }
+//             }
+//         ) {
+//             html,
+//             timeToRead,
+//             wordCount {
+//                 words
+//             }
+//         }
+//     }
+// `;
 
-const Blog = ({ data, pageContext }) => {
-    let parser = HtmlToReact.Parser;
-    parser = new parser();
-    const parsedHTML = parser.parse(data.markdownRemark.html);
-    return (
-        <div>
-            <div>
-                {data.markdownRemark.timeToRead} minutes to read
-            </div>
-            <div>
-                {parsedHTML}
-            </div>
-        </div>
-    );
+// const Blog = ({ data, pageContext }) => {
+//     let parser = HtmlToReact.Parser;
+//     parser = new parser();
+//     const parsedHTML = parser.parse(data.markdownRemark.html);
+//     return (
+//         <div>
+//             <div>
+//                 {data.markdownRemark.timeToRead} minutes to read
+//             </div>
+//             <div>
+//                 {parsedHTML}
+//             </div>
+//         </div>
+//     );
+// }
+
+const Blog = () => {
+    return <div>Blog placeholder</div>
 }
-
 export default Blog;
