665819a3f48023607900a65322f35f23664e6df0
Tim Zhang <timzhang3@gmail.com>
2022-04-03 13:39:56 +1000

    Implement basic parallax banner image

--- src/pages/cybersecurity.tsx
+++ src/pages/cybersecurity.tsx
@@ -1,18 +1,55 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { MarkdownRenderer } from 'src/components/markdown';
 import { CybersecurityLayout } from '../layouts';
 import styles from './cybersecurity.module.scss';
 import mdSource from 'src/portfolio-data/cybersecurity/test.md';
 
-const CybersecurityBlogs = ({ query }) => {
+const CybersecurityBlogs = () => {
+    const bannerRef = useRef(null);
+
+    // Parallax banner.
+    useEffect(() => {
+        const bannerImage = bannerRef.current;
+
+        const parallaxScroll = () => {
+            const offset = window.pageYOffset;
+            bannerImage.style.backgroundPositionY = offset * 0.7 + 'px';
+        };
+
+        window.addEventListener('scroll', parallaxScroll);
+
+        return () => {
+            window.removeEventListener('scroll', parallaxScroll);
+        };
+    }, []);
+
     return (
         <CybersecurityLayout>
             <Helmet>
                 <title>Cybersecurity Blogs</title>
             </Helmet>
             <div className={styles.container}>
-                <MarkdownRenderer mdSource={mdSource} />
+                <div className={styles.content}>
+                    <MarkdownRenderer mdSource={mdSource} />
+                </div>
+                <div
+                    ref={bannerRef}
+                    style={{
+                        height: '500px',
+                        width: '100%',
+                        backgroundImage:
+                            'url(https://images4.alphacoders.com/110/thumb-1920-1108171.png)',
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                        position: 'absolute',
+                        top: -10,
+                    }}
+                ></div>
+                {/* <img
+                    className={styles.bannerImage}
+                    src="https://images4.alphacoders.com/110/thumb-1920-1108171.png"
+                ></img> */}
             </div>
         </CybersecurityLayout>
     );
--- src/portfolio-data/cybersecurity/test.md
+++ src/portfolio-data/cybersecurity/test.md
@@ -8,3 +8,9 @@ if (test) {
     return 2;
 }
 ```
+
+Duis aute sunt Lorem laboris aute pariatur culpa aute sunt non irure. Occaecat eu irure et cillum nulla ea magna eu officia. Aliquip eiusmod cupidatat elit ea aute reprehenderit velit adipisicing aute. Aliquip nisi enim commodo aliquip adipisicing ullamco nulla anim amet dolore. Consequat voluptate eu laborum laborum aliquip incididunt occaecat incididunt. Ullamco officia laboris in quis ad nostrud minim anim proident. Ullamco ut consequat voluptate cupidatat ad.
+
+Aute excepteur ipsum commodo qui exercitation ex consequat nisi nisi. Nulla sunt quis veniam exercitation fugiat eu est. Id culpa quis nulla consectetur veniam aute irure est excepteur cupidatat qui enim dolor.
+
+Fugiat fugiat consequat laborum ut id adipisicing et amet cupidatat sint tempor. Anim irure mollit aliquip ad ad ad velit cupidatat duis. Reprehenderit eiusmod aute labore amet consequat aliquip aliqua minim dolore aliquip cupidatat eu veniam. Non voluptate qui quis dolore non. Velit sunt enim aliquip fugiat irure eu nisi excepteur consectetur pariatur ea nisi voluptate veniam.
