24376eade73edb5287d8ef6234d1a21cc5a8e48d
Tim Zhang <timzhang3@gmail.com>
2022-06-17 07:38:04 +1000

    Add sample robots.txt and boilerplate sitemap generator

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,14 @@
 import { motion } from "framer-motion";
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
 import { LinkButton } from "src/components/Button";
 import CentralContainer from "src/components/Container/CentralContainer";
 import ContentContainer from "src/components/Container/ContentContainer";
 import styles from "./404.module.scss";
 
 const NotFoundPage: NextPage = () => {
+    const router = useRouter();
+
     return (
         <motion.div
             initial={{ opacity: 0 }}
@@ -16,6 +19,10 @@ const NotFoundPage: NextPage = () => {
                 <ContentContainer centerContents>
                     <div className={styles.centered}>
                         <h1>404</h1>
+                        <p>
+                            Couldn't find &apos;{router && router.asPath}
+                            &apos;
+                        </p>
                         <LinkButton href="/" text="Go Home" />
                     </div>
                 </ContentContainer>
