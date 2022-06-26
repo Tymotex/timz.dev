f0baf9164ff577cd5e832db4e10538831e39af54
Tim Zhang <timzhang3@gmail.com>
2022-06-26 21:28:39 +1000

    Add medal icons for academic awards list UI.

--- src/components/Education/AwardItem.tsx
+++ src/components/Education/AwardItem.tsx
@@ -1,6 +1,7 @@
 import { Award } from "content/portfolio/portfolio";
 import React from "react";
 import styles from "./AwardItem.module.scss";
+import { CgAwards as AwardIcon } from "react-icons/cg";
 
 interface Props {
     award: Award;
@@ -9,9 +10,12 @@ interface Props {
 const AwardItem: React.FC<Props> = ({ award }) => {
     return (
         <li className={styles.awardItem}>
-            <h4 className={styles.title}>{award.title}</h4>
-            <small className={styles.date}>{award.date}</small>
-            <p className={styles.description}>{award.description}</p>
+            <AwardIcon size={32} className={styles.awardIcon} />
+            <div className={styles.details}>
+                <h4 className={styles.title}>{award.title}</h4>
+                <small className={styles.date}>{award.date}</small>
+                <p className={styles.description}>{award.description}</p>
+            </div>
         </li>
     );
 };
--- src/components/UnorderedList/UnorderedList.tsx
+++ src/components/UnorderedList/UnorderedList.tsx
@@ -3,11 +3,23 @@ import React from "react";
 
 interface Props {
     children: React.ReactNode;
+    resetDefaultStyle?: boolean;
 }
 
 // Very simple wrapper around <ul> with standard styling applied.
-const UnorderedList: React.FC<Props> = ({ children }) => {
-    return <ul className={styles.unorderedList}>{children}</ul>;
+const UnorderedList: React.FC<Props> = ({
+    children,
+    resetDefaultStyle = false,
+}) => {
+    return (
+        <ul
+            className={`${styles.unorderedList} ${
+                resetDefaultStyle && styles.reset
+            }`}
+        >
+            {children}
+        </ul>
+    );
 };
 
 export default UnorderedList;
