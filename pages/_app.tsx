b3bdb78478bd723fd0b74dc68d92370bb818e102
Tim Zhang <admin@timz.dev>
2024-08-26 18:47:03 +1000

    Instrument with Vercel-provided performance insights gathering.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@reach/combobox/styles.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import portfolio from "content/portfolio/portfolio";
 import { AnimatePresence } from "framer-motion";
 import type { AppProps } from "next/app";
@@ -45,6 +46,7 @@ const App = ({ Component, pageProps, router }: AppProps) => {
                 />
             </Head>
             <Analytics />
+            <SpeedInsights />
 
             {/* Only show the particle wallpaper when on blog routes. */}
             <AnimatePresence
